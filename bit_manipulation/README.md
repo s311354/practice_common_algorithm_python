841be78442979828bbeab950964b7a325d2e6910
s311354 <qazqazqaz850@gmail.com>
2022-03-20 13:45:46 +0800

    recap fundamental bit manipulation

---
+++ bit_manipulation/README.md
@@ -0,0 +1,11 @@
+
+## Population Count ##
+
+The population count of a specific value is the number of set bits in that value. Calculating the population count efficiently has been widely studied with implementation existing for both software and hardware. Many of the popular microprocessors also provide hardware support for this operation.
+
+## Reference ##
+- [cppreference: std::popcount](https://en.cppreference.com/w/cpp/numeric/popcount)
+- [popcount 算法分析](https://www.twblogs.net/a/5b8cf63d2b71771883379c33)
+- [Hacker's Delight 2nd Edition](https://www.amazon.com/Hackers-Delight-2nd-Henry-Warren/dp/0321842685)
+- [WikiChip: Population Count (popcount)](https://en.wikichip.org/wiki/population_count)
+- [text](https://address)
--- bit_manipulation/bits.py
+++ bit_manipulation/bits.py
@@ -1,4 +1,21 @@
 
+def get_bits(x, pos, n):
+    """docstring for get_bits"""
+#     print (~( ~0 << n))
+    mask = ~(~0 << n)
+    return ( x >> (pos + 1 - n) & mask)
+
+
+def is_power_of_2(x):
+    """docstring for is_power_of_2"""
+    return (x & x - 1) == 0
+
+
+def is_even(x):
+    """docstring for is_even"""
+    return x & 1 == 0
+
+
 def hanming_distance(x, y):
     """TODO: Docstring for hanming_distance."""
 
@@ -35,11 +52,14 @@ def hanming_weight(x):
     return count
 
 
-def pop_count(i):
-    """docstring for pop_count: Returns the number of 1 bit in the value of x"""
+def hacker_pop_count(i):
+    """docstring for hacker_pop_count: Returns the number of 1 bit in the value of i"""
+    """ hacker_popcnt """
     i -= ((i >> 1) & 0x55555555)
     i = (i & 0x33333333) + ((i >> 2 ) & 0x33333333)
+
     return (((i + (i >> 4) & 0xF0F0F0F) * 0x1010101) & 0xFFFFFFFF) >> 24
+
 
 def main():
     """docstring for main"""
@@ -53,10 +73,30 @@ def main():
     print("Bit 2: 1011 1100 set?", is_bit_set(0b1010111100, 2))
 
     print("Hamming weight: 1010111100:", hanming_weight(0b1010111100))
+    print("Hamming weight: 0b11111111:", hanming_weight(0b11111111))
 
-    print("PopConut: " + str(bin(1231424)) + ":", pop_count(1231424))
-    print("PopCount: " + str(bin(1)) + ":", pop_count(1))
-    print("PopCount: " + str(bin(8568)) + ":", pop_count(8568))
+    print("Hacker PopConut: " + str(bin(1231424)) + ":", hacker_pop_count(1231424))
+    print("Hacker PopCount: " + str(bin(1)) + ":", hacker_pop_count(1))
+    print("Hacker PopCount: " + str(bin(8568)) + ":", hacker_pop_count(8568))
+
+
+    for x in range(0, 5):
+        print(str(x) + 'is even?', is_even(x))
+
+
+    for x in range(0, 8):
+        print(str(x) + 'is power of 2?', is_power_of_2(x))
+
+
+    print("Get 3 bits starting at position 5 in 1010111100", bin(get_bits(0b1010111100, 5, 3)))
+    print("Get 3 bits starting at position 9 in 1010111100", bin(get_bits(0b1010111100, 9, 3)))
+
+    print("Get 4 bits starting at position 9 in 1010111100", bin(get_bits(0b1010111100, 9, 4)))
+
+
+
+
+
 
 
 
