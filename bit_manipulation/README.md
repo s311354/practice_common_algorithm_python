997336e1216bc982d62ee92bee074c8acff145d6
s311354 <qazqazqaz850@gmail.com>
2022-03-21 13:19:48 +0800

    Recap fundamental bit manipulation

--- bit_manipulation/README.md
+++ bit_manipulation/README.md
@@ -1,11 +1,38 @@
 
-## Population Count ##
+## Bit Manipulation ##
+
+
+#### Population Count ####
 
 The population count of a specific value is the number of set bits in that value. Calculating the population count efficiently has been widely studied with implementation existing for both software and hardware. Many of the popular microprocessors also provide hardware support for this operation.
 
-## Reference ##
+#### Setting a Bit ####
+
+Use the bitwise OR operator (|) to set a bit
+
+#### Clear a Bit ####
+
+Use the bitwise AND operator (&) to clear a bit
+
+#### Toggle a Bit ####
+
+Use the bitwise XOR operator (^) to toggle a bit
+
+#### Parity Check Bit ####
+
+is commonly used for the simplest scheme of error detection.
+
+#### Parity Bit ####
+
+In mathematics parity can refer to the evenness or oddness of an integer, which, when written in its binary form, can be determined just by examining only its least significant bit.
+
+#### Reference ####
+
 - [cppreference: std::popcount](https://en.cppreference.com/w/cpp/numeric/popcount)
 - [popcount 算法分析](https://www.twblogs.net/a/5b8cf63d2b71771883379c33)
 - [Hacker's Delight 2nd Edition](https://www.amazon.com/Hackers-Delight-2nd-Henry-Warren/dp/0321842685)
 - [WikiChip: Population Count (popcount)](https://en.wikichip.org/wiki/population_count)
-- [text](https://address)
+- [Stackoverflow: How do you set, clear, and toggle a single bit?](https://stackoverflow.com/questions/47981/how-do-you-set-clear-and-toggle-a-single-bit)
+- [GeeksforGeeks: Add two numbers without using arithmetic operators](https://www.geeksforgeeks.org/add-two-numbers-without-using-arithmetic-operators/)
+- [Parity Check Bit](https://www.sciencedirect.com/topics/computer-science/parity-check-bit)
+- [Wiki: Parity bit](https://en.wikipedia.org/wiki/Parity_bit)
--- bit_manipulation/bits.py
+++ bit_manipulation/bits.py
@@ -1,4 +1,94 @@
 
+def swap_ints(a, b):
+    """docstring for swap_ints"""
+    a ^= b
+    b ^= a
+    a ^= b
+    return [a, b]
+
+
+def myabs(x):
+    """docstring for myabs"""
+    # Two's complement conversion from negative to positive
+    high_bit_mask = x >> 31
+    return (x ^ high_bit_mask) - high_bit_mask
+
+def modulo(a, b):
+    """docstring for modulo"""
+    mask = ( (1 << b) - 1 )
+    return a & mask
+
+
+def next_power_of_2(x):
+    """docstring for next_power_of_2"""
+    x -= 1
+    x |= x >> 1
+    x |= x >> 2
+    x |= x >> 4
+    x |= x >> 8
+    x |= x >> 16
+    x += 1
+    return x
+
+
+def has_parity_parallel(x):
+    """docstring for has_parity_parallel"""
+    x ^= x >> 16
+    x ^= x >> 8
+    x ^= x >> 4
+    x  &= 0xf
+    return ( 0x6996 >> x) & 1
+
+
+# has odd number of bits
+def has_parity(x):
+    """docstring for has_parity"""
+    parity = False
+
+    while x:
+        parity = not parity
+        x &= (x - 1)
+
+    return parity
+
+
+def get_sign(x):
+    """docstring for get_sign"""
+    return -(x < 0)
+
+
+def add(a, b):
+    """docstring for add"""
+    while a:
+        carry = b&a
+        b ^= a # Sum of bits of a and b where at least one of the bits is not set
+#         print(carry << 1)
+        a = carry << 1 # Carry is shifted by one so that adding it to b gives the required sum
+    return b
+
+
+def rotate_left(x, n):
+    """docstring for rotate_left"""
+    # assumes a 32 bit word size
+    rotated_left = x >> (-n & 31)
+#     print(rotated_left)
+    return ( x << n | rotated_left)
+
+
+def clear_bit(x, position):
+    """docstring for clear_bit"""
+    return x & ~(1 < position)
+
+
+def set_bit(x , position):
+    """docstring for set_bit"""
+    return x | (1 << position)
+
+
+def toggle_bit(x, position):
+    return x ^ (1 << position)
+
+
 def get_bits(x, pos, n):
     """docstring for get_bits"""
 #     print (~( ~0 << n))
@@ -63,7 +153,7 @@ def hacker_pop_count(i):
 
 def main():
     """docstring for main"""
-    for x in range(0, 20):
+    for x in range(0, 32):
         print(x, bin(x), hex(x))
 
     print("Hamming distance: 1010111100 and 1001010101", hanming_distance(0b1010111100, 0b1001010101))
@@ -94,11 +184,43 @@ def main():
     print("Get 4 bits starting at position 9 in 1010111100", bin(get_bits(0b1010111100, 9, 4)))
 
 
+    print("Set bit 4 to 0: 01010010", bin(clear_bit(0b01010010, 4)))
+    print("Set bit 0 to 1: 01010010", bin(set_bit(0b01010010, 0)))
+    print("Toggle bit 1: 01010010", bin(toggle_bit(0b01010010, 1)))
+
+    print("Rotate left 5 positions: 01001000100101011001010010011011:",
+          bin(rotate_left(0b01001000100101011001010010011011, 5)))
 
 
+    print("3 + 5 = ", add(3, 5))
+    print("33 + 51 = ", add(33, 51))
+    print("40 + 90 = ", add(40, 90))
+
+    print("Sign of 5", get_sign(5))
+    print("Sign of 0", get_sign(0))
+    print("Sign of -1", get_sign(-1))
+    print("Sign of -3", get_sign(-3))
+
+    for x in range(0, 12):
+        print("has parity: " + str(x), has_parity(x), bin(x))
+
+    for x in range(0, 7):
+        print("has parity (parallel): " + str(x), has_parity_parallel(x), bin(x))
 
 
+    print("Next power of 2 after 3", next_power_of_2(3))
+    print("Next power of 2 after 4", next_power_of_2(4))
+    print("Next power of 2 after 45", next_power_of_2(45))
 
+    print("4 mod 2", modulo(4, 2))
+    print("5 mod 2", modulo(5, 2))
+    print("10 mod 3", modulo(10, 3))
+
+    print("abs -134", myabs(-134))
+    print("abs 99", myabs(99))
+
+    print("swap 1, 3", swap_ints(1, 3))
+    print("swap 213, 14", swap_ints(213, 14))
 
 if __name__ == "__main__":
     main()
