1b5a2e312ba331d230d925625bc3af76d9ad692b
s311354 <qazqazqaz850@gmail.com>
2022-04-27 22:13:09 +0800

    Create unittest before any class are created, and by definition you should only test a single unit in isolation.

--- leetcode/mock_test.py
+++ leetcode/mock_test.py
@@ -19,6 +19,35 @@ class SolutionCase(unittest.TestCase):
         expected_output = [1, 2]
         self.assertEqual(solution.twoSum(nums, target), expected_output)
 
+    def test_minNumberOfSemesters(self):
+        """docstring for solution"""
+        solution = Solution()
+        n = 4
+        dependencies = [[2, 1], [3, 1], [1, 4]]
+        k = 2
+
+        expected_output = 3
+#         self.assertEqual(solution.minNumberOfSemesters(n, dependencies, k), expected_output)
+
+        n = 12
+        dependencies = [[1, 2], [1, 3], [7, 5], [7, 6], [4, 8], [8, 9], [9, 10], [10, 11], [11, 12]]
+        k = 2
+
+        expected_output = 6
+#         self.assertEqual(solution.minNumberOfSemesters(n, dependencies, k), expected_output)
+
+        n = 13
+        dependencies = [[12,8],[2,4],[3,7],[6,8],[11,8],[9,4],[9,7],[12,4],[11,4],[6,4],[1,4],[10,7],[10,4],[1,7],[1,8],[2,7],[8,4],[10,8],[12,7],[5,4],[3,4],[11,7],[7,4],[13,4],[9,8],[13,8]]
+        k = 9
+        expected_output = 3
+#         self.assertEqual(solution.minNumberOfSemesters(n, dependencies, k), expected_output)
+
+        n = 14
+        dependencies = [[11,7]]
+        k = 2
+        expected_output = 7
+        self.assertEqual(solution.minNumberOfSemesters(n, dependencies, k), expected_output)
+
 
 def main():
     """docstring for main"""
--- leetcode/solution.py
+++ leetcode/solution.py
@@ -1,4 +1,6 @@
 from typing import List
+import collections
+from functools import lru_cache
 
 
 class Solution:
@@ -11,3 +13,84 @@ class Solution:
                 store[nums[i]] = i
             else:
                 return [store[sec], i]
+
+    def count1(self, number: int) -> int:
+        ones = 0
+        tmp = number
+        while tmp > 0:
+            if tmp & 1 != 0:
+                ones += 1
+            tmp >>= 1
+        return ones
+
+    def combinations(self, number: int, k: int) -> List[int]:
+        """
+        :param number: a number that will be treated in binary form
+        :param k: number of ones in the result integers
+        :return: a list of integers with no more than k 1s
+        """
+
+        def helper(current_number: int, num_ones: int, remain_1s: int) -> List[int]:
+            if remain_1s == 0:
+                return [0]
+            if num_ones <= remain_1s:
+                return [current_number]
+            else:
+                # `(current_number - 1) ^ current_number` will give us all 1s starting from the smallest `1`
+                last_one = ((current_number - 1) ^ current_number) & current_number
+
+                # For choosing the last `1`
+                p1 = helper(current_number - last_one, num_ones=num_ones - 1, remain_1s=remain_1s - 1)
+
+                # For not choosing the last `1`
+                p2 = helper(current_number - last_one, num_ones=num_ones - 1, remain_1s=remain_1s)
+
+                return [p + last_one for p in p1] + p2
+
+        return helper(current_number=number, num_ones=self.count1(number), remain_1s=k)
+
+    def minNumberOfSemesters(self, n: int, dependencies: List[List[int]], k: int) -> int:
+
+        # Compute in-degree and adjacency graph for each node
+        in_degree = [0] * n
+        graph = collections.defaultdict(list)
+        for prerequisite, course in dependencies:
+            graph[prerequisite - 1].append(course - 1)
+            in_degree[course - 1] += 1
+
+        @lru_cache(None)
+        def find_min_semester(courses: int) -> int:
+            if not courses:
+                return 0
+
+            combinations = self.combinations(courses, k)
+
+            min_semester = 1 << 32 # MAXIMUM
+            for k_courses in combinations:
+                remaining_courses = courses - k_courses
+
+                next_courses = 0
+
+                # Decrease the in-degree
+                for course_idx in range(n):
+                    if (1 << course_idx) & k_courses:
+                        for nei in graph[course_idx]:
+                            in_degree[nei] -= 1
+                            if in_degree[nei] == 0:
+                                next_courses += 1 << nei
+
+                min_semester = min(min_semester, find_min_semester(remaining_courses + next_courses))
+
+                # Increase the in-degree (backtracking)
+                for course_idx in range(n):
+                    if (1 << course_idx) & k_courses:
+                        for nei in graph[course_idx]:
+                            in_degree[nei] += 1
+            return min_semester + 1
+
+        initial_courses = 0
+        for i in range(n):
+            if in_degree[i] == 0:
+                initial_courses += 1 << i
+
+        return find_min_semester(initial_courses)
