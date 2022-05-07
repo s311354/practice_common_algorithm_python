55c1631383f203b2ad3c03016968ab977851e489
s311354 <qazqazqaz850@gmail.com>
2022-05-07 21:33:41 +0800

    Correct the description

--- leetcode/__init__.py
+++ leetcode/__init__.py
@@ -1,17 +1,11 @@
-"""Routines for working with triangles.
+"""Routines for working with leetcode practice
 
-The two modules inside of this package are packed with useful features
-for the programmer who needs to support triangles:
+The two modules inside of this package are packed with useful features:
 
-``shape``
-    This module provides a full-fledged `Triangle` object that can be
-    instantiated and then asked to provide all sorts of information
-    about its properties.
+``solution``
+    This module provides a `Solution` object that is the implementation corresponding to each of quizs.
 
-``utils``
-    For the programmer in a hurry, this module offers quick functions
-    that take as arguments the three side lengths of a triangle, and
-    perform a quick computation without the programmer having to make
-    the extra step of creating an object.
+``mock_test``
+    For unit test, this module uses unit testing framework to supports test automation, sharing of setup and shutdown code for tests.
 
 """
